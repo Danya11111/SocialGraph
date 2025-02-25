ab3f1b41d76f1b2a0c0a4c1c1b0733da18533d63
danilka_arkhipov <daniil.arhipov.2005@gmail.com>
2025-02-25 11:19:52 +0300

    Added files

---
+++ SocialGraph/SocialGraph/Graph.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class Graph {
+	
+
+	vector<vector<string>>
+};
---
+++ SocialGraph/SocialGraph/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Graph.h"
+#include "program.h"
+using namespace std;
+
+int main() {
+	prog();
+}
